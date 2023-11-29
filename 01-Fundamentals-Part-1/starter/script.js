a614078a95f4e5fec59ac10c969c092fd7482ea0
Milo <5724020+mylow@users.noreply.github.com>
2023-11-29 09:31:16 -0800

    chapter 2

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,251 @@
+// let js = 'amazing';
+// if (js  === 'amazing') alert('JavaScript is FUN!')
+
+// 40 + 8 + 23 - 10
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Milo');
+// console.log(25);
+
+// let firstName = "Milo"
+
+// console.log(firstName);
+
+// Assignment 1:
+
+// let country = 'USA'
+// let continent = 'Americas'
+// let population = 1002000000
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let isIsland = country
+// if ((isIsland === country) (console.log('true'))
+
+// let language
+
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Milo');
+
+// javascriptIsFun = 'YES!'
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30
+// age = 31
+
+// const birthYear = 1998
+// birthYear = 1999;
+
+// var job = 'programmer'
+// job = 'teacher'
+// console.log(job);
+
+// let x, y;
+// x = y = 25-10-5
+// console.log(x, y);
+
+// const firstName = 'Milo'
+// const job = 'front end dev'
+// const birthYear = 1998
+// const year = 2023
+
+// const milo = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'
+// console.log(milo);
+
+// const miloNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+// console.log(miloNew);
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if(age >= 18) {
+//     console.log('Sarah can start her driving license 🚙 ');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young, she must wait ${yearsLeft} years`);
+// }
+
+// const birthYear = 2012;
+// let century
+
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(`${century}`);
+
+// // type conversion
+// const inputYear = '1991'
+
+// console.log(Number(inputYear), inputYear)
+// console.log(Number(inputYear) + 18)
+
+// console.log(Number('Jonas'));
+// console.log(typeof(NaN));
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log('I am '+ 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' + '10' + 3);
+// console.log('23' * '2');
+// console.log('23' / '2');
+
+// let n = '1' + 1;
+// n = n - 1
+// console.log(n);
+
+// console.log(Boolean(0));        // false
+// console.log(Boolean(undefined));// false
+// console.log(Boolean('Milo'));   // true
+// console.log(Boolean({}));       // true
+// console.log(Boolean(''));       // false
+
+// const money = 100
+// if(money) {
+//     console.log("don't spend it all");
+// } else {
+//     console.log("You should get a job");
+// }
+
+// const age = '18'
+// if(age === 18) console.log('you are an adult (strict)');
+
+// if(age == 18) console.log('you are an adult (loose)');
+
+// const favorite = Number(prompt("What's your favorite number?"));
+// console.log(favorite);
+// console.log(typeof favorite);
+
+// if (favorite === 23) {
+//   console.log("Cool! 23 is a good number");
+// } else if (favorite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favorite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7");
+// }
+
+// if (favorite !== 23) console.log('Why not 23?');
+
+// const hasDriversLicense = true // A
+// const hasGoodVision = true // B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision
+
+// if(hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah should be able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// const isTired = true
+
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah should be able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// const day = 'monday'
+
+// switch(day) {
+//     case 'monday':  // day === 'monday'
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break
+//     case 'friday':
+//         console.log('Record videos');
+//         break
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend!');
+//         break
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+// if(day === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples');
+// } else if (day === 'friday') {
+//     console.log('Record videos');
+// } else if (day === 'saturday' || day === 'sunday') {
+//     console.log('Enjoy the weekend!');
+// } else {
+//     console.log('Not a valid day!');
+// }
+
+const age = 17;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water'
+
+console.log(drink);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
